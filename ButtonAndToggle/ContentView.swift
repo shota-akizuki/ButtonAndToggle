060698007ad714db2c79cff709cea3af2fc1141f
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-01-10 21:50:17 +0900

    Initial Commit

---
+++ ButtonAndToggle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonAndToggle
+//
+//  Created by 秋月翔太 on 2021/01/10.
+//  Copyright © 2021 Shota Akizuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
